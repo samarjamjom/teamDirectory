c41f4d55f9ccda0fce8ea7647fca326c22dd38f3
Samar Jamjom <samarjamjom@gmail.com>
2019-10-12 23:46:32 +0300

    listShow

--- teamDir/teamDir.js
+++ teamDir/teamDir.js
@@ -1,38 +1,6 @@
 /*
-let teamMember = {
-    name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    major : document.getElementById("major").value,
-    role : document.getElementById("role").value,
-    Bio : document.getElementById("bio").value
-};
-
-
 let teamDir = [];
-teamDir.push(teamMember);
 
-let submit = document.getElementById("save");
-submit.onclick = saveTeamMember(teamDir);
-
-function saveTeamMember(teamDir){
-    const jsonString = JSON.stringify(teamDir);
-    localStorage.setItem('teamList',jsonString);
-}
-
-const fromStoragte = get();
-
-function get() {
-    const fromstorage = localStorage.getItem('teamList');
-    return fromstorage ? JSON.parse(fromstorage) : [];
-}
-
-
-document.getElementById("list-members").innerHTML= fromStoragte.innerHTML;
-
-console.log(fromStoragte);
-
-*/
-let teamDir = [];
 function myFunction() { 
     let teamMember = {
         name : document.getElementById("name").value,
@@ -42,7 +10,7 @@ function myFunction() {
         Bio : document.getElementById("bio").value
     };
     teamDir.push(teamMember);
-    const jsonString = JSON.stringify(teamDir);
+    const jsonString = JSON.stringify(teamDir); // string representation of obj to store it in local storage
     localStorage.setItem('teamList',jsonString);
     
     let s = get();
@@ -53,3 +21,42 @@ function get() {
     const fromstorage = localStorage.getItem('teamList');
     return fromstorage ? JSON.parse(fromstorage) : [];
 }
+*/
+
+const ul = document.querySelector('ul');
+let teamDir = localStorage.getItem('teamList') ? JSON.parse(localStorage.getItem('teamList')) : [];
+//console.log(teamDir);
+
+localStorage.setItem('teamList', JSON.stringify(teamDir));
+const data = JSON.parse(localStorage.getItem('teamList'));
+//console.log(data);
+
+const liMaker = text => {
+    const li = document.createElement('li');
+    
+    li.textContent = text.name  + "\n" +text.email + 
+    console.log( li.textContent);
+    ul.appendChild(li);
+  }
+
+
+function myFunction() { 
+    let teamMember = {
+        name : document.getElementById("nam").value,
+        email : document.getElementById("email").value,
+        major : document.getElementById("major").value,
+        role : document.getElementById("role").value,
+        Bio : document.getElementById("bio").value
+    };
+    teamDir.push(teamMember);
+    localStorage.setItem('teamList', JSON.stringify(teamDir));
+   
+    nam.value = '';
+    email.value = '';
+    bio.value = '';
+} 
+
+data.forEach(item => {
+    liMaker(item)
+  })
+
